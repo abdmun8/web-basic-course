ecf1c6af8f5f399fdf2dc688d97d8136e4e3adaf
abdmun8@gmail.com <abdmun8@gmail.com>
2020-08-30 13:11:01 +0700

    add javascript dom

---
+++ javascript-dom/materi.js
@@ -0,0 +1,19 @@
+//
+document.getElementById("nama-id");
+document.getElementsByClassName("ini-class");
+document.querySelector(".ini-class"); //("#ini-class");
+document.querySelectorAll(".ini-class");
+document.getElementsByTagName("ol");
+
+{
+  /* <div id="nama-id" class="ini-class"></div>;
+<div class="ini-class"></div>;
+
+.nama-class{
+  border: blue;
+}
+
+#id{
+
+} */
+}
--- javascript/review.js
+++ javascript/review.js
@@ -3,14 +3,40 @@
  * ['asep','dani']
  */
 
+let arr1 = [1, 2, 3, 4, 5];
+let arr2 = ["dani", "asep", "dede", "jaja", "doni"];
+
 /**
- * buat array dengan panjang 100, isi bebas, number | boolean | string
+ * buat array dengan panjang 500, isi bebas, number | boolean | string
  * [1,2,3]
  */
 
+let arr = createArray(500);
+
+function createArray(length = 0) {
+  let newArr = [];
+  for (let index = 0; index < length; index++) {
+    newArr.push(index);
+  }
+  return newArr;
+}
+
+// console.log(arr);
+
 /**
  * buat object dari array tersebut
- * key adalah nama
+ * key adalah `nama`
  * contoh array ['asep','dani']
  * hasil object [{nama: 'asep'},{nama:'dani'}]
  */
+
+function createObjArr(arrInput) {
+  let arr = [];
+  arrInput.forEach((element) => {
+    arr.push({ nama: element });
+  });
+  return arr;
+}
+
+let arrObj = createObjArr(arr2);
+console.log(arrObj);
