33ebca97f85cfb7712fcc8f34b5c0a1eec6ce35e
abdmun8@gmail.com <abdmun8@gmail.com>
2020-08-30 09:24:00 +0700

    update /javascript/review.js

---
+++ javascript/review.js
@@ -0,0 +1,16 @@
+/**
+ * buat array dengan panjang 5, isi bebas, number | boolean | string
+ * ['asep','dani']
+ */
+
+/**
+ * buat array dengan panjang 100, isi bebas, number | boolean | string
+ * [1,2,3]
+ */
+
+/**
+ * buat object dari array tersebut
+ * key adalah nama
+ * contoh array ['asep','dani']
+ * hasil object [{nama: 'asep'},{nama:'dani'}]
+ */
