7ea23031965506520a5ffaa6910aba29486e3166
abdmun8@gmail.com <abdmun8@gmail.com>
2020-08-30 14:02:32 +0700

    add latihan event

--- javascript-dom/latihan/event/app.js
+++ javascript-dom/latihan/event/app.js
@@ -2,11 +2,26 @@ function gantiBackground(o) {
   o.style.backgroundColor = "red";
 }
 
-let orang = document.querySelector(".orang");
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+var orang = document.querySelector(".orang");
 orang.addEventListener("click", function () {
   alert("aduh saya di klik");
 });
 
 orang.addEventListener("mouseover", function () {
   gantiBackground(this);
+});
+
+var button = document.querySelector("button");
+button.addEventListener("click", function () {
+  var color = getRandomColor();
+  orang.style.backgroundColor = color;
 });
