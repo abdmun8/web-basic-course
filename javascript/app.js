a8dfcc8cf093ab01d0a2f686c15143f326361b25
abdmun8@gmail.com <abdmun8@gmail.com>
2020-08-30 09:20:24 +0700

    initial commit

---
+++ javascript/app.js
@@ -0,0 +1,96 @@
+// var a = 'nama';
+// let b = 'kelas';
+// const c = 'hadir';
+// c = 'tidak hadir';
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(buka)
+
+// function perkalian(angka){
+//     var pengali = 567;
+//     var pembagi = 67532;
+//     var pengurang = 65;
+//     return angka * pengali / pembagi - pengurang;
+// }
+
+// var d = perkalian(30);
+// var e = perkalian(50);
+// var f = perkalian(70);
+
+// console.log('nilai d adalah '+ d,'nilai e adalah '+e, 'nilai f adalah '+f)
+
+// function ucapkanSalam(nama){
+//     return 'selamat datang '+ nama;
+// }
+
+// console.log(ucapkanSalam('asep'));
+// console.log(ucapkanSalam('dani'));
+
+// var tebakNama = (nama,kelas) => `nama kamu adalah ${nama}, kelas ${kelas}`;
+// var tebakNama = (nama,kelas) => "nama kamu adalah "+nama+", kelas "+kelas;
+// var tampilkanTitle = x = "<h1 class=\"\" id=''>"+x+"</h1>"
+
+/*
+console.log(tebakNama('dodo', '5A'))
+console.log(tebakNama('bima'))
+*/
+// let arr = ['nama', 'alamat', 1, true];
+// console.log(arr);
+var orang = 20;
+// var orang2 = 20;
+
+
+// if(orang <11){
+//     console.log('dia anak-anak')
+// }else if(orang >=11 && orang <= 20){
+//     console.log('dia remaja')
+// }else{
+//     console.log('dia dewasa')
+// }
+{/* <><=>=
+== */}
+// ===
+// var a = null;
+// var b = '';
+// if(a == b && ){
+//     console.log('sama')
+// }else{
+//     console.log('tidak sama')
+// }
+// var a = false; // 1
+// var b = false; // 2
+// var c = false; // 3
+
+// if(a){
+//     console.log('a')
+// }else if(!a&&!b&&c){
+//     console.log('c')
+// }else if(!a&&b&&!c){
+//     console.log('b')
+// }else{
+//     console.log('tidak ada jalan')
+// }
+// var a = 1&2;
+// console.log(a)
+// var umur = 5;
+// var d = umur < 11 ? 'anak-anak' : umur < 20 ? 'remaja' : 'dewasa';
+// console.log(d)
+// var cff = 0;
+// setInterval(function(){
+//     var body = document.getElementById('main');
+//     if(cff % 2 == 0){
+//         body.style.backgroundColor = 'green';
+//     }else{
+//         body.style.backgroundColor = 'yellow';
+//     }
+//     cff++;
+// },1000)
+
+
+
+
+
+
+
+
